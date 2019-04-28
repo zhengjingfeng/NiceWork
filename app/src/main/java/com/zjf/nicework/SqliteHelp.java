32e25462f54a742b659d534ff1d957b295c02730
zjf <15919820315@163.com>
2019-04-28 22:47:41 +0800

    4

--- app/src/main/java/com/zjf/nicework/SqliteHelp.java
+++ app/src/main/java/com/zjf/nicework/SqliteHelp.java
@@ -3,7 +3,11 @@ package com.zjf.nicework;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.github.yuweiguocn.library.greendao.MigrationHelper;
 import com.zjf.nicework.bean.DaoMaster;
+import com.zjf.nicework.bean.UserDao;
+
+import org.greenrobot.greendao.database.Database;
 
 /**
  * @author jingfeng
@@ -12,8 +16,21 @@ import com.zjf.nicework.bean.DaoMaster;
  */
 public class SqliteHelp extends DaoMaster.OpenHelper {
 
-
     public SqliteHelp(Context context, String name, SQLiteDatabase.CursorFactory factory) {
         super(context, name, factory);
+    }
+
+    @Override
+    public void onUpgrade(Database db, int oldVersion, int newVersion) {
+        MigrationHelper.migrate(db, new MigrationHelper.ReCreateAllTableListener() {
+            @Override
+            public void onCreateAllTables(Database db, boolean ifNotExists) {
+                DaoMaster.createAllTables(db, ifNotExists);
+            }
+            @Override
+            public void onDropAllTables(Database db, boolean ifExists) {
+                DaoMaster.dropAllTables(db, ifExists);
+            }
+        }, UserDao.class);
     }
 }
